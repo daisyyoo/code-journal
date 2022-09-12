4d66cd5e7bb64f540b2ca02ed140c07ab2c22bcb
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-12 23:09:23 +0000

    submit button finished and object prepend finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var $photoURL = document.querySelector('#photo-url');
-$photoURL.addEventListener('input', insertPhoto);
+var $newEntry = document.querySelector('form');
+var $previewPhoto = document.querySelector('img');
+var $photoURL = document.querySelector('#photo');
+$photoURL.addEventListener('input', photoURLBox);
 
-function insertPhoto(event) {
-  $photoURL.setAttribute('src', event.target.value);
-
+function photoURLBox(event) {
+  $previewPhoto.setAttribute('src', $newEntry.elements.photo.value);
 }
+
+function newEntry(event) {
+  event.preventDefault();
+  var title = $newEntry.elements.title.value;
+  var photoURL = $newEntry.elements.photo.value;
+  var notes = $newEntry.elements.notes.value;
+  var newEntryObject = {
+    title,
+    photoURL,
+    notes,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(newEntryObject);
+  $previewPhoto.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $newEntry.reset();
+}
+
+$newEntry.addEventListener('submit', newEntry);
